9f327f8c804fdba92e442d559109ff1158e01e92
radoslav-tsvetanov-themuse <radoslav.tsvetanov@themuse.com>
2026-05-27 00:22:12 +0300

    .

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 export { type RouteTree, type IRouteHandler } from "./src/core/server";
-export { type Hook, Hooks, type HooksDefault } from "./src/core/hooks"
+export { Hook, Hooks, type HooksDefault } from "./src/core/hooks"
 export { RouterObject } from "./src/core/server/router/Router";
 export * from "./src/core/server/router/routeMatcher"
 export type {RequestData} from "./src/core/server/types"
